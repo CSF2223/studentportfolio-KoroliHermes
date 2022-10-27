00d19cfb5739f803433ff884cba3cc263be947b2
EC2 Default User <ec2-user@ip-172-31-55-89.ec2.internal>
2022-10-27 22:09:37 +0000

    Complete through lesson 4.04

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+    navbarLinks.classList.toggle('active');
+});
